452de612a4acfdf622f1826e61b9cd3483181ac7
Corentin-Vidick <corentin.vidick@gmail.com>
2022-10-14 17:53:30 +0000

    checking boat, not owrking yet

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,7 @@ function playGame() {
     let button = document.createElement("button");
     button.innerHTML = "Play";
     document.getElementById("menu").appendChild(button);
-    button.addEventListener("click", buildPhase)
+    button.addEventListener("click", buildPhase);
 }
 
 function createMap() {
@@ -25,10 +25,10 @@ function createMap() {
 }
 
 function buildPhase() {
-    document.getElementById("menu").innerHTML = `<button id="boat1">Boat 0</button>
-    <button id="boat2">Boat 1</button>`;
+    document.getElementById("menu").innerHTML = `<button id="boat1">Boat 1</button>
+    <button id="boat2">Boat 2</button>`;
     for (let x = 50 ; x <=99 ; x++) {
-        document.getElementById(x).className="cell fog";
+        document.getElementById(x).classList.add ("fog");
     }
     let buttons = document.getElementsByTagName("button");
     for (let button of buttons) {
@@ -44,19 +44,77 @@ function buildPhase() {
 
 function placeBoat() {
 
-    document.getElementById("menu").innerHTML = `<h2>Place boat 1</h2>`
+    document.getElementById("menu").innerHTML = `<h2>Place boat 1</h2><button>OK</button>`;
     let cells = document.getElementsByClassName("cell");
     for (let cell of cells) {
-        cell.addEventListener("click", function(){this.className = "cell boat";})
+        cell.addEventListener("mouseover", higlightPlacement);
+        cell.addEventListener("mouseout", normal);
+        cell.addEventListener("click", function(){
+            if (this.className === "cell boat"){
+                this.classList.remove ("boat");
+            } else if (this.className === "cell highlight-cell"){
+                this.classList.add ("boat");
+            }
+        })            
     }
-    checkPlacement()
+    let ok = document.getElementsByTagName("button");
+    ok[0].addEventListener("click", checkPlacement);
 }
 
 function checkPlacement() {
-
-    for (x = 0 ; x < 100 ; x ++) {
-        if (document.getElementById(x) === "boat") {
-            console.log("checked first boat appearance");
+    alert("Boat placed");
+    let boatCounter = 0;
+    let lastEncounter = [];
+    for (let x = 0 ; x < 50 ; x ++) {
+        if (document.getElementById(x).className === "cell boat") {
+            lastEncounter[boatCounter] = x;
+            boatCounter ++ ;
         }
     }
+    if (boatCounter != 4){
+        alert("Wrong length of boat! Please try again and select four cells");
+        placeBoat();
+    } else for (let x=0 ; x<(boatCounter-1) ; x++) {
+        if ((lastEncounter[x]%10) === (lastEncounter[x+1]%10) 
+            && (lastEncounter[x+1]%10) === (lastEncounter[x+2]%10)
+            && (lastEncounter[x+2]%10) === (lastEncounter[x+3]%10)) {
+                alert("correct boat horizontal");
+        } else if ( document.getElementById(lastEncounter[x]).className === "cell boat"
+                    && document.getElementById(lastEncounter[x]).className === "cell boat"
+                    && document.getElementById(lastEncounter[x]).className === "cell boat") {        //check if in-line vertical
+            alert("correct boat vertical"); 
+        } else {
+            alert("incorrect placement");
+        }
+    }
+    
+    /*if ( document.getElementById(lastEncounter-10).className === "cell boat"
+                && document.getElementById(lastEncounter-20).className === "cell boat"
+                && document.getElementById(lastEncounter-30).className === "cell boat") {        //check if in-line vertical
+        alert("correct boat vertical");      
+    } else if ( ((lastEncounter - 3) % 10) < ((lastEncounter-2) % 10)
+                && ((lastEncounter- 2) % 10) < ((lastEncounter-1) % 10)
+                && ((lastEncounter- 1) % 10) < (lastEncounter % 10)
+                && (lastEncounter - (lastEncounter % 10)) === ((lastEncounter-1) - ((lastEncounter-1) % 10))
+                && ((lastEncounter-1) - ((lastEncounter-1) % 10)) === ((lastEncounter-2) - ((lastEncounter-2) % 10))
+                && ((lastEncounter-2) - ((lastEncounter-2) % 10)) === ((lastEncounter-3) - ((lastEncounter-3) % 10)) ) {
+        alert("correct boat horizontal");
+    } else {
+        alert("Boat not in a straight line");
+        placeBoat();
+    }  */
+}
+
+// Effects
+function higlightPlacement() {
+    if (this.className === "cell") {
+        this.classList.add ("highlight-cell");
+    } else if (this.className === "cell fog") {
+        this.classList.add ("highlight-fog");
+    }                
+}
+
+function normal() {
+    this.classList.remove("highlight-cell");
+    this.classList.remove("highlight-fog");
 }
