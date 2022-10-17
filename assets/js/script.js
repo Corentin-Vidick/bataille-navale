ea54820e5d2d629860e5d45601bff699133a5976
Corentin-Vidick <corentin.vidick@gmail.com>
2022-10-17 12:38:59 +0000

    both players can put boats, need to check all boats placed to move on

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,109 +4,167 @@ document.addEventListener("DOMContentLoaded", function() {
 })
 
 function runGame() {
-    createMap();
+    //createMap();
     playGame();
 }
 
-function playGame() {
-    let button = document.createElement("button");
-    button.innerHTML = "Play";
-    document.getElementById("menu").appendChild(button);
-    button.addEventListener("click", buildPhase);
-}
-
-function createMap() {
+/**
+ * Creates contents of game-area
+ */
+function createMap(player) {
+    document.getElementById("game-container").innerHTML = ``;
     for (let x = 0 ; x < 100 ; x++) {
         let cell = document.createElement("div");
         cell.className = "cell";
         cell.id = x;
         document.getElementById("game-container").appendChild(cell);
     }
+    if (player === 1) {
+        createMap();
+        for (let x = 50 ; x <=99 ; x++) {
+            document.getElementById(x).classList.add ("fog");   // fogs player 2's half of map
+        }
+    } else if (player === 2) {
+        createMap();
+        for (let x = 0 ; x <=49 ; x++) {
+            document.getElementById(x).classList.add ("fog");   // fogs player 1's half of map
+        } 
+    }
 }
 
+/**
+ * Menu page
+ */
+function playGame() {
+    document.getElementById("game-container").innerHTML = 
+        `<button id="play">Play</button>`;
+    document.getElementById("play").addEventListener("click", buildPhase);
+}
+
+/**
+ * Select player to place boats or ready to go to next phase
+ */
 function buildPhase() {
-    document.getElementById("menu").innerHTML = `<button id="boat1">Boat 1</button>
-    <button id="boat2">Boat 2</button>`;
-    for (let x = 50 ; x <=99 ; x++) {
-        document.getElementById(x).classList.add ("fog");
-    }
+    let ready1 = false;
+    let ready2 = false;
+    document.getElementById("menu").innerHTML = 
+        `<button id="ready">Ready</button>`;
+    document.getElementById("game-container").innerHTML = 
+        `<button id="player1">Player 1</button>
+        <button id="player2">Player 2</button>`;
     let buttons = document.getElementsByTagName("button");
     for (let button of buttons) {
-        button.addEventListener("click", function(){
-            if (this.id === "boat1"){
-                placeBoat();
-            } else if (this.id === "boat2") {
-                placeBoat();
+        button.addEventListener("click", function() {
+            if (this.id === "player1") {
+                createMap(1);
+                placeBoat(1, 1);
+            } else if (this.id === "player2") {
+                createMap(1);
+                placeBoat(2, 1);
+                ready2 = true;
+            } else if (this.id === "ready") {
+                if (ready1 && ready2) {
+                    battlePhase();
+                } else {
+                    alert("please place all boats");
+                }
+
             }
         })
     }
 }
 
-function placeBoat() {
+function placeBoat(player, boat) {
+    if (boat === 3) {
+        buildPhase();
+    } else {
+//create contents of page    
+    document.getElementById("menu").innerHTML = `<h2>Player ${player}, place boat ${boat}</h2><button id="ok">OK</button>`;    
+//boat placement
+        let cells = document.getElementsByClassName("cell");
+        for (let cell of cells) {
+            cell.addEventListener("mouseover", higlightPlacement);
+            cell.addEventListener("mouseout", normal);
+            cell.addEventListener("click", function(){
+                if (this.className === "cell boat"){
+                    this.classList.remove ("boat");
+                } else if (this.className === "cell highlight-cell"){
+                    this.classList.add ("boat");
+                }
+            })            
+        }
+        let correct = 0;
+        document.getElementById("ok").addEventListener("click", function () {
+                correct = parseInt(checkPlacement());
 
-    document.getElementById("menu").innerHTML = `<h2>Place boat 1</h2><button>OK</button>`;
-    let cells = document.getElementsByClassName("cell");
-    for (let cell of cells) {
-        cell.addEventListener("mouseover", higlightPlacement);
-        cell.addEventListener("mouseout", normal);
-        cell.addEventListener("click", function(){
-            if (this.className === "cell boat"){
-                this.classList.remove ("boat");
-            } else if (this.className === "cell highlight-cell"){
-                this.classList.add ("boat");
-            }
-        })            
+        if (correct === 1) {
+            boat ++;
+            confirmBoat(player,boat);
+        }
+    });
     }
-    let ok = document.getElementsByTagName("button");
-    ok[0].addEventListener("click", checkPlacement);
-}
+} 
 
 function checkPlacement() {
     alert("Boat placed");
+    let correct = 0;
     let boatCounter = 0;
-    let lastEncounter = [];
-    let incorrect = 0;
-    for (let x = 0 ; x < 50 ; x ++) {
+    let x = 0;
+    rowLoop:
+        while (x < 50) {
+            let boatInRow = 0;
+            let y = 0;
+            while (y < 10){
+                if (document.getElementById(x+y).className === "cell boat") {
+                    boatInRow ++;
+                }
+                y ++;
+            }
+            if (boatInRow === 4) {
+                correct = 1;
+                break rowLoop;
+            }
+            x += 10;
+        }
+    let y = 0;
+    rowColumn:
+        while (y < 10) {
+            let boatInColumn = 0;
+            let x = 0;
+            while (x < 50) {
+                if (document.getElementById(x+y).className === "cell boat") {
+                    boatInColumn ++;
+                }
+                x += 10;
+            }
+            if (boatInColumn === 4) {
+                correct = 1;
+                break rowColumn;
+            }
+            y ++;
+        }
+    for (let x = 0; x < 50 ; x++){
         if (document.getElementById(x).className === "cell boat") {
-            lastEncounter[boatCounter] = x;
-            boatCounter ++ ;
+            boatCounter ++;
         }
     }
-    if (boatCounter != 4){
-        alert("Wrong length of boat! Please try again and select four cells");
-        placeBoat();                        // doesn't work as intended
-    } else for (let x=0 ; x<(boatCounter-1) ; x++) {
-        if ((lastEncounter[x]+1) !== (lastEncounter[x+1]) 
-            && parseInt(lastEncounter[x]/10) !== parseInt(lastEncounter[x+1]/10)) {
-                //alert("correct boat horizontal");
-                incorrect = 1;
-        } else if ((lastEncounter[x]+10) !== (lastEncounter[x+1])) {        //check if in-line vertical
-            //alert("correct boat vertical"); 
-            incorrect = 1;
-        } 
-    }
-    if (incorrect === 0) {
-        alert("good placement, move on");
+    if (boatCounter != 4) {correct = 0}
+    if (correct === 1) {
+        alert("Well done, moving on");
     } else {
-        alert("incorrect placement, start again");
-        placeBoat();
+        alert("wrong placement, please try again");
     }
-    
-    /*if ( document.getElementById(lastEncounter-10).className === "cell boat"
-                && document.getElementById(lastEncounter-20).className === "cell boat"
-                && document.getElementById(lastEncounter-30).className === "cell boat") {        //check if in-line vertical
-        alert("correct boat vertical");      
-    } else if ( ((lastEncounter - 3) % 10) < ((lastEncounter-2) % 10)
-                && ((lastEncounter- 2) % 10) < ((lastEncounter-1) % 10)
-                && ((lastEncounter- 1) % 10) < (lastEncounter % 10)
-                && (lastEncounter - (lastEncounter % 10)) === ((lastEncounter-1) - ((lastEncounter-1) % 10))
-                && ((lastEncounter-1) - ((lastEncounter-1) % 10)) === ((lastEncounter-2) - ((lastEncounter-2) % 10))
-                && ((lastEncounter-2) - ((lastEncounter-2) % 10)) === ((lastEncounter-3) - ((lastEncounter-3) % 10)) ) {
-        alert("correct boat horizontal");
-    } else {
-        alert("Boat not in a straight line");
-        placeBoat();
-    }  */
+    return [correct];
+}
+
+function confirmBoat(player, boat) {
+    for (let x = 0; x < 50 ; x++){
+        if (document.getElementById(x).className === "cell boat") {
+            document.getElementById(x).classList.add("confirmed");
+        }
+    }
+    alert("Boat confirmed");
+    placeBoat(player, boat);
 }
 
 // Effects
